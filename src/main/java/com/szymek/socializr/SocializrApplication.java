093e641610268c8efa73e5d4f16c46e1f702b2f8
zonkje <szymon.radziszewski93@gmail.com>
2022-02-12 14:50:39 +0100

    initial project add. close #1

---
+++ src/main/java/com/szymek/socializr/SocializrApplication.java
@@ -0,0 +1,13 @@
+package com.szymek.socializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szymek/socializr/SocializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymek.socializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
