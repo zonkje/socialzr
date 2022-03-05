d4bbf00d40d822d8367826523a180a2b578824a5
zonkje <szymon.radziszewski93@gmail.com>
2022-03-05 17:48:28 +0100

    add auditioning. close #23

--- src/main/java/com/szymek/socializr/SocializrApplication.java
+++ src/main/java/com/szymek/socializr/SocializrApplication.java
@@ -2,8 +2,10 @@ package com.szymek.socializr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SocializrApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/szymek/socializr/model/BaseEntity.java
+++ src/main/java/com/szymek/socializr/model/BaseEntity.java
@@ -2,12 +2,13 @@ package com.szymek.socializr.model;
 
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 
 @Getter
 @Setter
@@ -17,12 +18,17 @@ import java.io.Serializable;
 @SuperBuilder
 @MappedSuperclass
 @EqualsAndHashCode
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity implements Serializable {
-
-    //TODO: -add create date -add last modification date
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    private Instant createDate;
+
+    @LastModifiedDate
+    private Instant lastModifiedDate;
 
 }
